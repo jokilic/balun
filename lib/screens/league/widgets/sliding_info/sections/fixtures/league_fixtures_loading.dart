b1e53b4a9b002b5c375a6228ec9cfd0714c4be21
Josip Kilić <neksuses@gmail.com>
2024-08-27 14:48:12 +0200

    League teams states

--- lib/screens/league/widgets/sliding_info/sections/fixtures/league_fixtures_loading.dart
+++ lib/screens/league/widgets/sliding_info/sections/fixtures/league_fixtures_loading.dart
@@ -1,38 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
+import '../../../../../../constants.dart';
 import '../../../../../../theme/theme.dart';
 
 class LeagueFixturesLoading extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => ListView(
-        shrinkWrap: true,
-        padding: const EdgeInsets.fromLTRB(16, 8, 16, 40),
-        physics: const BouncingScrollPhysics(),
-        children: List.generate(
-          8,
-          (index) => Container(
-            margin: const EdgeInsets.symmetric(
-              horizontal: 4,
-              vertical: 12,
-            ),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 40,
-              vertical: 16,
-            ),
-            height: 64,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: context.colors.black,
-                width: 2,
+  Widget build(BuildContext context) => Animate(
+        onPlay: (controller) => controller.loop(
+          reverse: true,
+          min: 0.6,
+        ),
+        effects: const [
+          FadeEffect(
+            curve: Curves.easeIn,
+            duration: BalunConstants.shimmerDuration,
+          ),
+        ],
+        child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(16, 8, 16, 40),
+          physics: const BouncingScrollPhysics(),
+          children: List.generate(
+            8,
+            (index) => Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 4,
+                vertical: 12,
               ),
-              borderRadius: BorderRadius.circular(16),
-              color: context.colors.black.withOpacity(0.075),
-            ),
-            child: Container(
-              height: 24,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40,
+                vertical: 16,
+              ),
+              height: 64,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: context.colors.black.withOpacity(0.15),
+                border: Border.all(
+                  color: context.colors.black,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(16),
+                color: context.colors.black.withOpacity(0.075),
+              ),
+              child: Container(
+                height: 24,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: context.colors.black.withOpacity(0.15),
+                ),
               ),
             ),
           ),
---
+++ lib/screens/league/widgets/sliding_info/sections/teams/league_teams_loading.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+import '../../../../../../constants.dart';
+import '../../../../../../theme/theme.dart';
+import '../../../../../../util/color.dart';
+import '../../../../../../widgets/balun_seperator.dart';
+
+class LeagueTeamsLoading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Animate(
+        onPlay: (controller) => controller.loop(
+          reverse: true,
+          min: 0.6,
+        ),
+        effects: const [
+          FadeEffect(
+            curve: Curves.easeIn,
+            duration: BalunConstants.shimmerDuration,
+          ),
+        ],
+        child: ListView.separated(
+          shrinkWrap: true,
+          padding: const EdgeInsets.fromLTRB(16, 8, 16, 40),
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: 8,
+          itemBuilder: (_, __) => Container(
+            color: Colors.transparent,
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            child: Row(
+              children: [
+                Container(
+                  height: 56,
+                  width: 56,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: getRandomBalunColor(context),
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 24,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: context.colors.black.withOpacity(0.25),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Container(
+                        height: 16,
+                        width: 88,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: context.colors.black.withOpacity(0.15),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Container(
+                        height: 12,
+                        width: 144,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: context.colors.black.withOpacity(0.25),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          separatorBuilder: (_, __) => const BalunSeperator(),
+        ),
+      );
+}
