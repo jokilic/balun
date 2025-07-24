4c63d57f052cbc6862db20fa2f8ab93c12f2af3b
Josip Kilić <neksuses@gmail.com>
2025-07-24 22:18:39 +0200

    New web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4185685503"
+    serviceWorkerVersion: "2773845032"
   }
 });
