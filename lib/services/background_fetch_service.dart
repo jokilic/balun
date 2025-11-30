2ceb23ca8d63c7b22396f82201226d6dc3471db3
Josip Kilić <neksuses@gmail.com>
2025-11-30 01:18:09 +0100

    Bigger fetch interval

--- lib/services/background_fetch_service.dart
+++ lib/services/background_fetch_service.dart
@@ -55,7 +55,7 @@ class BackgroundFetchService {
     /// Configure [BackgroundFetch]
     await BackgroundFetch.configure(
       BackgroundFetchConfig(
-        minimumFetchInterval: 15,
+        minimumFetchInterval: 20,
         startOnBoot: true,
         stopOnTerminate: false,
         enableHeadless: true,
